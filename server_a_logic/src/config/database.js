4a6fe368986aa7880bf16d4014e880bf4391216d
Derick Mazelli Barbosa <derickmazellibarbosa@gmail.com>
2026-04-03 10:33:30 -0300

    Ajustando .env config e unificando variáveis de banco de dados

--- server_a_logic/src/config/database.js
+++ server_a_logic/src/config/database.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host:     process.env.SERVER_B_IP    || '172.17.0.1',
+  host:     process.env.DB_HOST     || 'argus-mysql',
   port:     parseInt(process.env.DB_PORT) || 3306,
   user:     process.env.DB_USER     || 'argus',
   password: process.env.DB_PASSWORD || 'argus@2024',
-  database: process.env.DB_NAME     || 'ArgusDB',
+  database: process.env.DB_NAME     || process.env.DB_DATABASE || 'ArgusDB',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
